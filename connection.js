e0b6feaebd337339488880c880b3b87e224481ad
URBAN DECO <agencemarketing@urbancommunication.net>
2023-06-09 10:55:23 +0100

    updating ...

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   host: "localhost",
   user: "postgres",
-  port: 5433,
+  port: 5432,
   password: "Urbancom2022",
   database: "Menutium-sat",
 });
